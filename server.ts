6bf31497826d6779212014b0fb1a1e17964c77fe
blackhatcat0x <hello@blackhatcat.com>
2025-10-23 13:35:19 +0100

    Fix for not building app on heroku

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 // server.ts - Next.js Standalone + Socket.IO
-import { setupSocket } from '@/lib/socket';
 import { createServer } from 'http';
 import next from 'next';
 import { Server } from 'socket.io';
+import { setupSocket } from './src/lib/socket';
 
 const dev = process.env.NODE_ENV !== 'production';
 const currentPort = parseInt(process.env.PORT || '3000', 10);
